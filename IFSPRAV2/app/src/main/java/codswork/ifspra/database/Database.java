aad9b0bb5d5249f87ac9abf226bf43e7d12b449e
JulianoZ <juliano_z@yahoo.com.br>
2016-08-08 17:20:43 -0300

    IFSPRAV2
    Gerenciamento de pedido e mesa

---
+++ IFSPRAV2/app/src/main/java/codswork/ifspra/database/Database.java
@@ -0,0 +1,62 @@
+package codswork.ifspra.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Felipe on 12/07/2016.
+ */
+public class Database extends SQLiteOpenHelper{
+
+    private static final int db_version = 1;
+
+    private Database(Context context) {
+        super(context, "db", null,db_version );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query = "CREATE TABLE IF NOT EXISTS product(" +
+                           "id INTEGER PRIMARY KEY, " +
+                           "name VARCHAR(45)," +
+                           "description VARCHAR(200)," +
+                           "price FLOAT(10)," +
+                           "short_description VARCHAR(50)," +
+                           "stock INT(10)," +
+                           "featured INT(1)," +
+                           "weight FLOAT(10)," +
+                           "picture VARCHAR(60)," +
+                           "picture2 VARCHAR(60)," +
+                           "subcat_id INT(5)," +
+                           "product_purchased INT(5)" + //1- already purchased 0-> No purchased
+                       ");";
+
+        db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+
+    private static Database sInstance;
+    //SINGLETON PARAMETERS
+    public static synchronized Database getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new Database(context.getApplicationContext());
+        }
+        return sInstance;
+    }
+
+    /* ########################
+       #         TOOLS        #
+       ######################## */
+
+    public static int getDataInt(String column, Cursor cursor){ return cursor.getInt(cursor.getColumnIndex(column)); }
+    public static String getDataString(String column, Cursor cursor){ return cursor.getString(cursor.getColumnIndex(column)); }
+    public static float getDataFloat(String column, Cursor cursor){ return cursor.getFloat(cursor.getColumnIndex(column)); }
+
+
+}
